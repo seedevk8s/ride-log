f5bbb2fdc1f9871a77193202e44894a5d44374bd
dcuIntelligentWeb <dcuIntelligentWeb@gmail.com>
2022-07-10 17:02:47 +0900

    ride-log first commit

---
+++ src/main/java/kr/co/ezenac/ridelog/RideLogApplication.java
@@ -0,0 +1,13 @@
+package kr.co.ezenac.ridelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/ezenac/ridelog/RideLogApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.ezenac.ridelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
