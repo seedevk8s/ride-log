a9d5f09150b0c566815d6ab96cfc0546bd9b55af
seedevk8s <seedevk8s@gmail.com>
2022-07-10 22:09:33 +0900

    SpringApplicationBuilder 클래스로 좀전 SpringApplication로 생성한것과 동일한 결과가 나오게 구현함.

--- src/main/java/kr/co/ezenac/ridelog/RideLogApplication.java
+++ src/main/java/kr/co/ezenac/ridelog/RideLogApplication.java
@@ -3,6 +3,7 @@ package kr.co.ezenac.ridelog;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.boot.web.context.WebServerPortFileWriter;
 
@@ -10,13 +11,22 @@ import org.springframework.boot.web.context.WebServerPortFileWriter;
 public class RideLogApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World Ezen!");
-		//SpringApplication.run(RideLogApplication.class, args);
+//		System.out.println("Hello, World Ezen!");
+//		//SpringApplication.run(RideLogApplication.class, args);
+//		
+//		SpringApplication application = new SpringApplication(RideLogApplication.class);
+//		application.setBannerMode(Banner.Mode.OFF);
+//		application.addListeners(new ApplicationPidFileWriter(), new WebServerPortFileWriter());
+//		application.run(args);
 		
-		SpringApplication application = new SpringApplication(RideLogApplication.class);
-		application.setBannerMode(Banner.Mode.OFF);
-		application.addListeners(new ApplicationPidFileWriter(), new WebServerPortFileWriter());
-		application.run(args);
+		
+		
+		new SpringApplicationBuilder(RideLogApplication.class)
+			.bannerMode(Banner.Mode.OFF)
+			.listeners(
+					new ApplicationPidFileWriter(),
+					new WebServerPortFileWriter())
+			.build().run(args);
 	}
 
 }
